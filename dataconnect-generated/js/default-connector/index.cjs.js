cded205639a1a85b65f2e972952fdd5cdeb5f326
Aaradhya Deotale <102856132+AaradhyaDeotale@users.noreply.github.com>
2025-03-21 13:28:27 +0530

    Delete dataconnect-generated/js/default-connector directory

--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { getDataConnect, validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'TripDarzee-Next-Gen-Travel-Planner-main',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
-
